70756465bf364eb54928e705ad424a97c85153bf
napcxy <napcxy@outlook.com>
2022-07-12 10:55:51 +0800

    feat: improve license plate page

--- src/views/license_plate/index.vue
+++ src/views/license_plate/index.vue
@@ -1,9 +1,94 @@
 <script lang="ts" setup>
+import { Plus } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+
+import type { UploadProps, UploadUserFile } from 'element-plus'
+
+const fileList = ref<UploadUserFile[]>([
+  {
+    name: 'food.jpeg',
+    url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+  },
+  {
+    name: 'plant-1.png',
+    url: '/images/plant-1.png'
+  },
+  {
+    name: 'food.jpeg',
+    url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+  },
+  {
+    name: 'plant-2.png',
+    url: '/images/plant-2.png'
+  },
+  {
+    name: 'food.jpeg',
+    url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+  },
+  {
+    name: 'figure-1.png',
+    url: '/images/figure-1.png'
+  },
+  {
+    name: 'food.jpeg',
+    url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+  },
+  {
+    name: 'figure-2.png',
+    url: '/images/figure-2.png'
+  }
+])
+
+const dialogImageUrl = ref('')
+const dialogVisible = ref(false)
+
+const handleRemove: UploadProps['onRemove'] = (uploadFile, uploadFiles) => {
+  console.log(uploadFile, uploadFiles)
+}
+
+const handlePictureCardPreview: UploadProps['onPreview'] = (uploadFile) => {
+  dialogImageUrl.value = uploadFile.url!
+  dialogVisible.value = true
+}
 </script>
 
 <template>
   <h1>Vehicle Plate</h1>
+
+  <div class="common-layout">
+    <el-container>
+      <el-main>
+        <el-upload
+          v-model:file-list="fileList"
+          action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+          list-type="picture-card"
+          :on-preview="handlePictureCardPreview"
+          :on-remove="handleRemove"
+        >
+          <el-icon><Plus /></el-icon>
+        </el-upload>
+
+        <el-dialog v-model="dialogVisible">
+          <img w-full :src="dialogImageUrl" alt="Preview Image" />
+        </el-dialog>
+      </el-main>
+      <el-aside width="400px">
+        <el-space direction="vertical">
+          <el-card v-for="i in 2" :key="i" class="box-card" style="width: 250px">
+            <template #header>
+              <div class="card-header">
+                <span>Card name</span>
+                <el-button class="button" text>Operation button</el-button>
+              </div>
+            </template>
+            <div v-for="o in 4" :key="o" class="text item">
+              {{ 'List item ' + o }}
+            </div>
+          </el-card>
+        </el-space>
+      </el-aside>
+    </el-container>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
